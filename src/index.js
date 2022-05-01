13f8389c8c80cce088c943874dc63ade32349beb
Rayan Syed <syedray@sheridancollege.ca>
2022-04-30 22:00:42 -0400

    addded post functionality

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk';
 import App from './App';
 
 import reducers from "./reducers";
-const store = createStore(reducers, compose(applyMiddleware(thunk)))
-
+// const store = createStore(reducers, compose(applyMiddleware(thunk)))
+const store = configureStore({reducer: reducers})
 ReactDOM.render(
     <Provider store={store}>
         <App/>
