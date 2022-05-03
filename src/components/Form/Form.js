13453d7873e05a8d54154a7a6f23ab4f9dc33818
Rayan Syed <syedray@sheridancollege.ca>
2022-05-03 18:34:32 -0400

    added delete post functionality

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -24,9 +24,12 @@ const Form = ({ currentId, setCurrentId }) => {
     } else{
       dispatch(createPost(postData))
     }
+    clear();
   }
 
   const clear = () => {
+    setCurrentId(null);
+    setPostData({creator: '', title: '', message: '', tags: '', selectedFile: ''});
   }
   return (
     <Paper className='{classes.paper}'>
